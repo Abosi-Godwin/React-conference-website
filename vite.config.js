64ff30b3995fad0811ef6fa3ab4a3f15ceee2a69
Abosi-Godwin <bbnl6060@github.com>
2024-12-27 10:49:18 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { babel } from '@rollup/plugin-babel';
+import { babel } from "@rollup/plugin-babel";
 export default defineConfig({
-    plugins: [
-        react()
-    ],
+    plugins: [react()],
     server: {
         port: 8000
     },
-  build: {
-    rollupOptions: {
-      plugins: [
-        babel({
-          babelHelpers: 'bundled', // Ensures Babel is bundled with the project
-          exclude: 'node_modules/**', // Avoid transpiling all of node_modules
-        }),
-      ],
-    },
-  },
-  
+    build: {
+        rollupOptions: {
+            plugins: [
+                babel({
+                    babelHelpers: "bundled", // Ensures Babel is bundled with the project
+                    exclude: "node_modules/**" // Avoid transpiling all of node_modules
+                })
+            ]
+        }
+    }
 });
- 
